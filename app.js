28add5a4854344973fff2f91fa2cc1a106068bbe
Ishaan <ishaan10dutta@gmail.com>
2022-02-16 13:32:27 +0530

    Added basic bootstrap styling.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {});
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -14,7 +19,7 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
--- models/campground.js
+++ models/campground.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 const CampGroundSchema = new Schema({
     title: String,
-    price: String,
     description: String,
+    image: String,
+    price: Number,
     location: String
 });
 
